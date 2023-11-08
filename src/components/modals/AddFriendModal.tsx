ecae975d430842a11d3ded1602a748ad7c465e0d
RT <rustytrim11@gmail.com>
2023-11-07 21:49:53 -0500

    User Status and Friend Request
    Co-authored-by: Bryden Calaway <BrydenIsNotSmart@users.noreply.github.com>
    Co-authored-by: ForGetFulSkyBro <forgetfulskybro@users.noreply.github.com>

--- src/components/modals/AddFriendModal.tsx
+++ src/components/modals/AddFriendModal.tsx
@@ -1,9 +1,11 @@
 import { Dispatch, FormEvent, SetStateAction, useEffect, useState } from "react";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import { useClient } from "@/context/ClientContext";
 
 export default function AddFriendModal({ show, set }: { show: boolean, set: Dispatch<SetStateAction<boolean>> }) {
     const [isBrowser, setIsBrowser] = useState(false);
+    const { client } = useClient();
     const [query, setQuery] = useState("");
 
     useEffect(() => {
@@ -24,18 +26,20 @@ export default function AddFriendModal({ show, set }: { show: boolean, set: Disp
         }
     }, [set]);
 
-    const handleSubmit = () => {
-
+    const handleSubmit = async (event: FormEvent) => {
+        event.preventDefault();
+        const result = query.split("#");
+        client?.addFriend(result[0], result[1]);
     }
 
     return (
-        <form onSubmit={(event) => event.preventDefault()} id="backdrop" className="absolute w-full h-full left-0 top-0 bg-[rgba(0,0,0,0.75)] z-[999] flex justify-center items-center">
+        <form onSubmit={(event) => handleSubmit(event)} id="backdrop" className="absolute w-full h-full left-0 top-0 bg-[rgba(0,0,0,0.75)] z-[999] flex justify-center items-center">
             <div className="w-64 p-4 bg-stone-900 flex flex-col gap-2 rounded-lg">
                 <h2>Add Friend</h2>
                 <Input placeholder="username#9999" type="text" value={query} onChange={(event) => setQuery(event.target.value)} />
                 <div className="flex justify-end gap-2">
-                    <Button onClick={() => set(false)} className="bg-stone-800 p-1 rounded-lg">Cancel</Button>
-                    <Button onClick={() => handleSubmit()} className="bg-stone-800 p-1 rounded-lg">Send</Button>
+                    <Button type={"button"} onClick={() => set(false)} className="bg-stone-800 p-1 rounded-lg">Cancel</Button>
+                    <Button type={"submit"} className="bg-stone-800 p-1 rounded-lg">Send</Button>
                 </div>
             </div>
         </form>
--- src/context/ClientContext.tsx
+++ src/context/ClientContext.tsx
@@ -1,27 +1,29 @@
 "use client";
 
-import { Client } from "strafe.js";
+import { Client, Friend } from "strafe.js";
 import {
   Dispatch,
   SetStateAction,
   createContext,
-  use,
   useContext,
   useEffect,
   useState,
 } from "react";
 import { usePathname } from 'next/navigation'
 import cookie from "js-cookie";
-import { error } from "console";
 import LoadingScreen from "@/components/LoadingScreen";
 
 interface IClientContext {
   client?: Client;
+  friends?: Friend[],
   ready?: boolean;
   serverListPos?: string;
+  status?: string;
   setClient?: Dispatch<SetStateAction<Client | undefined>>;
+  setFriends?: Dispatch<SetStateAction<Friend[]>>;
   setReady?: Dispatch<SetStateAction<boolean>>;
-  setServerListPos?: Dispatch<SetStateAction<string>>
+  setServerListPos?: Dispatch<SetStateAction<string>>;
+  setStatus?: Dispatch<SetStateAction<string>>;
 }
 
 const ClientContext = createContext<IClientContext>({});
@@ -29,9 +31,11 @@ const ClientContext = createContext<IClientContext>({});
 export const ClientProvider = ({ children }: { children: JSX.Element }) => {
   const pathname = usePathname();
   const [client, setClient] = useState<Client>();
+  const [friends, setFriends] = useState<Friend[]>([])
   const [ready, setReady] = useState(false);
   const [clientError, setClientError] = useState(false);
   const [serverListPos, setServerListPos] = useState("");
+  const [status, setStatus] = useState("");
 
   useEffect(() => {
     if (pathname.startsWith("/signup") || pathname.startsWith("/login")) return;
@@ -42,22 +46,30 @@ export const ClientProvider = ({ children }: { children: JSX.Element }) => {
       newClient.on("ready", () => {
         if (newClient.user) setServerListPos(newClient.user.preferences!.serverListPos);
         setReady(true);
+        setStatus(newClient.user?.status.name!);
+        newClient.getFriends().then((friends) => {
+          if (friends != null) setFriends(friends);
+        });
         setClientError(false);
-      })
+      });
     } else if (!client.options.token) client.login(cookie.get("token")!)
   }, [client, pathname]);
 
   useEffect(() => {
     if (pathname.startsWith("/signup") || pathname.startsWith("/login")) return;
     client?.on("error", () => {
-       setClientError(true);
+      setClientError(true);
+    })
+
+    client?.on("presenceUpdate", (data) => {
+      setStatus(data);
     })
   }, [client])
 
   if (clientError) return <LoadingScreen />
-   
+
   return (
-    <ClientContext.Provider value={{ client, serverListPos, ready, setClient, setReady, setServerListPos }}>
+    <ClientContext.Provider value={{ client, friends, serverListPos, status, ready, setClient, setFriends, setReady, setServerListPos, setStatus }}>
       {children}
     </ClientContext.Provider>
   );
